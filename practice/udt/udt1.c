c83035f8a3e65a2cb85046ca078a36af02a985cc
Sanketh T S <sankethts2003@gmail.com>
2026-05-07 05:55:17 +0000

    initial commit

--- practice/udt/udt1.c
+++ practice/udt/udt1.c
@@ -173,3 +173,26 @@ int main()
     return 0;
 }
 #endif
+
+#if 0
+//another way to check little or big endian
+union Endian
+{
+    unsigned int value;
+    unsigned char byte[4];
+};
+int main()
+{
+    union Endian e={0x12345678};
+    e.byte[0] == 0x78 ? printf("Little\n") : printf("Big\n");
+    return 0;
+}
+#endif
+
+#if 0
+//
+int main()
+{ 
+
+}
+#endif

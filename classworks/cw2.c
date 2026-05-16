e7826a5122284247689c1dcb8eb4795e3a1a95f2
Sanketh T S <sankethts2003@gmail.com>
2026-05-16 15:09:50 +0000

    initial commit

--- classworks/cw2.c
+++ classworks/cw2.c
@@ -1,10 +1,12 @@
-//W.A.P to print the power of two series using for loop
+//W.A.P to print the n power of two series using for loop
 
 #include<stdio.h>
 int main()
 {
+    int n;
+    scanf("%d", &n);
     int j=1;
-    for (int i=1; i<=10; i++)
+    for (int i=1; i<=n; i++)
     {
         printf("%d ", j);
         j=j*2;

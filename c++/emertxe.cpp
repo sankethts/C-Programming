180a200d6d3acc9c60a0fc66d53fdb4987580d8f
Sanketh T S <sankethts2003@gmail.com>
2026-05-18 15:40:33 +0000

    initial commit

---
+++ c++/emertxe.cpp
@@ -0,0 +1,11 @@
+#include "emertxe.h"
+int main()
+{
+    Emertxe e(123, "sanketh", "shivamogga");
+    Mentor m(456, "Pravin", "Mysore", "CPP", "Senior");
+    Candidate c(789, "Anand", "Mumbai", "ECEP", 2026);
+    e.display();
+    m.display();
+    c.display();
+    return 0;
+}

6c60bdacba1a9d1fac591955d5d29f2655c66604
Sanketh T S <sankethts2003@gmail.com>
2026-05-25 05:10:21 +0000

    initial commit

--- c++/emertxe.cpp
+++ c++/emertxe.cpp
@@ -1,11 +1,15 @@
 #include "emertxe.h"
 int main()
 {
-    Emertxe e(123, "sanketh", "shivamogga");
-    Mentor m(456, "Pravin", "Mysore", "CPP", "Senior");
-    Candidate c(789, "Anand", "Mumbai", "ECEP", 2026);
-    e.display();
-    m.display();
-    c.display();
+    // Emertxe e(123, "sanketh", "shivamogga");
+    // Mentor m(456, "Pravin", "Mysore", "CPP", "Senior");
+    // Candidate c(789, "Anand", "Mumbai", "ECEP", 2026);
+
+    Emertxe *e = new Emertxe(123, "sanketh", "shivamogga");
+    Emertxe *m = new Mentor(456, "Pravin", "Mysore", "CPP", "Senior");
+    //Candidate c(789, "Anand", "Mumbai", "ECEP", 2026);
+    e->display();
+    m->display();
+    //c.display();
     return 0;
 }
